91fea56f140655a3a9d574a78316d2f890af12c3
Tomasz Karpiej <tomash342@gmail.com>
2024-07-26 13:54:48 +0200

    Created project

---
+++ src/main/java/com/example/RepoLister/RepoListerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RepoLister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoListerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoListerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RepoLister/RepoListerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RepoLister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoListerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
